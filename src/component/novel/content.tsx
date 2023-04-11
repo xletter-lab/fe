57cf997384fea234f82315f60988fcdb143e665f
MyunghaCho <chomyungha@gmail.com>
2023-04-11 18:01:49 +0900

    forwardRef 수정

--- src/component/novel/content.tsx
+++ src/component/novel/content.tsx
@@ -6,38 +6,39 @@ type Props = {
   data: ContentDetailType;
   clickOption: (targetContentId: number, newData: ContentDetailType) => void;
 };
-const Content = forwardRef(
-  ({ clickOption, data }: Props, ref: ForwardedRef<HTMLDivElement>) => {
-    const selectOption = (selectOptionId: number) => {
-      clickOption(data.contentIndex, {
-        ...data,
-        options: data.options.map((option) => {
-          return {
-            ...option,
-            isSelected: selectOptionId === option.optionId,
-          };
-        }),
-      });
-    };
-    return (
-      <div ref={ref}>
-        <div>{data.text}</div>
-        <div>
-          {data.options?.map((option, index) => {
-            return (
-              <Option
-                key={`option_${data.contentIndex}_${option.optionId}`}
-                optionType={option}
-                optionIndex={index}
-                clickOption={selectOption}
-                isFirst={option.isFirst}
-              />
-            );
-          })}
-        </div>
+const Content = (
+  { clickOption, data }: Props,
+  ref: ForwardedRef<HTMLDivElement>
+) => {
+  const selectOption = (selectOptionId: number) => {
+    clickOption(data.contentIndex, {
+      ...data,
+      options: data.options.map((option) => {
+        return {
+          ...option,
+          isSelected: selectOptionId === option.optionId,
+        };
+      }),
+    });
+  };
+  return (
+    <div ref={ref}>
+      <div>{data.text}</div>
+      <div>
+        {data.options?.map((option, index) => {
+          return (
+            <Option
+              key={`option_${data.contentIndex}_${option.optionId}`}
+              optionType={option}
+              optionIndex={index}
+              clickOption={selectOption}
+              isFirst={option.isFirst}
+            />
+          );
+        })}
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
 Content.displayName = "Content";
-export default Content;
+export default forwardRef(Content);
