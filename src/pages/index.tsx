ef1be6f0e66a7614b4fccbee50b1ae07700060f6
MyunghaCho <chomyungha@gmail.com>
2023-04-11 18:35:57 +0900

    원시상태

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import Welcome from "@/component/main/Welcome";
-import Info from "@/component/main/Info";
-import Footer from "@/component/main/Footer";
 import styles from "./index.module.css";
 
 export default function Main() {
@@ -23,15 +20,5 @@ export default function Main() {
     );
   };
 
-  return (
-    <div className={styles.page}>
-      <Welcome
-        email={email}
-        changeEmail={changeEmail}
-        clickReadig={clickNext}
-      />
-      <Info email={email} changeEmail={changeEmail} clickReadig={clickNext} />
-      <Footer />
-    </div>
-  );
+  return <div className={styles.page}>메인</div>;
 }
